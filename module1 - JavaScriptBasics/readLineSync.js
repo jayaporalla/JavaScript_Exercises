c97b4f3a33aa85254b5adc24586615447fcd3fe9
Jayasri P <jayaporalla921@gmail.com>
2025-11-01 20:41:58 +0530

    Made changes in module1

--- module1 - JavaScriptBasics/readLineSync.js
+++ module1 - JavaScriptBasics/readLineSync.js
@@ -2,8 +2,8 @@
 // user and console
 const readlinesync = require("readline-sync");
 
-const userName = readlinesync.question("What is your name?");
-const userAge = readlinesync.question("How old are you?");
+const userName = readlinesync.question("What is your name? ");
+const userAge = readlinesync.question("How old are you? ");
 console.log(`Thank you for entering your details...
 Your name:- ${userName}
 Your Age:- ${userAge}`);
