cf88a00db54bfd351bc3662ee61566bb62326004
Jayasri P <jayaporalla921@gmail.com>
2025-11-01 21:32:47 +0530

    changes made in module2

--- module2 - JavaScriptFundamentals/comparisonOperators.js
+++ module2 - JavaScriptFundamentals/comparisonOperators.js
@@ -9,4 +9,4 @@ console.log(null > 0); //false
 console.log(null < 1); //true bcz null converts to 0 when we do comparison with numbers.
 
 console.log(null === undefined); // false - strict quality check
-console.log("7" === 7); //false
+console.log("7" === 7); //false. In Strict equality it will check value and type both.
